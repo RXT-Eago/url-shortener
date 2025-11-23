d272fddcdc2cac9af6c1d0e07fe7650afbbe5266
clement delbarre <clement.delbarre@edu.ece.fr>
2025-11-23 21:30:50 +0100

    init backend + overall architecture for the answer

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # url-shortener
 A solution to create a url shortner service
+
+
+# Architecture
+
+## Front/UI
+
+> I decided to go with NextJS: a framework i use a lot and that can be easily deployed with Vercel
+
+## Back/API
+
+> A simple way to answer the issue: easy to confure, initiate and deploy.
+
+## DB
+
+> Classic SQL DB will do the job (but scalability can become an issue...)
---
+++ url-shortener-api/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import type { Request, Response } from "express";
+
+// configures dotenv to work in your application
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 5050;
+
+app.get("/", (request: Request, response: Response) => { 
+  response.status(200).send("Hello World");
+}); 
+
+app.listen(PORT, () => { 
+  console.log("Server running at PORT: ", PORT); 
+}).on("error", (error) => {
+  // gracefully handle error
+  throw new Error(error.message);
+});
